a7c3b4dbcb4a583f1674c82fbf2eb1cf67d13910
onlyoneuche <daleentontech@gmail.com>
2023-01-10 00:31:57 +0100

    update signature

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Bamidele Adefolaju
+# Author - Uchechukwu Emmanuel
 
 def islower(c):
     """Function checks for lowercase characters."""
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Bamidele Adefolaju
+# Author - Uchechukwu Emmanuel
 
 def uppercase(str):
     """Print a string in uppercase."""
